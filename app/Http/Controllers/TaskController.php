9d62d7c00d2ecbc3a8b1b7e0ec1562c33abc04af
Desire <angeboutchoue@gmail.com>
2020-06-27 13:00:08 +0300

    commit

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Task;
+use Illuminate\Http\Request;
+
+class TaskController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        return Task::all();
+    }
+
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+        $task = new Task();
+        $task->content = $request->content;
+        $task->active = $request->active;
+
+        $task->save();
+
+       return $task;
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Task  $task
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+        return Task::find($id);
+    }
+
+
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Task  $task
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+
+        $task = Task::find($id);
+        $task->content = $request->content;
+        $task->active = $request->active;
+
+        $task->save();
+
+       return $task;
+
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Task  $task
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+        $task = Task::find($id);
+        $task->delete();
+        
+        return response()->json([]);
+    }
+}
---
+++ database/factories/TaskFactory.php
@@ -0,0 +1,14 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Task;
+use Faker\Generator as Faker;
+
+$factory->define(Task::class, function (Faker $faker) {
+    return [
+        //
+        'content' => $faker->sentence(),
+        'active' => $faker->boolean()
+    ];
+});
